7e85dc3e92538ad4af0cf533fddd97b5e7b5074b
Balazs Ercsey <balazs.ercsey@elementary-interactive.com>
2023-01-19 15:43:37 +0100

    All the basics.
    #NEON-24

---
+++ src/Models/Base.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Neon\Models;
+
+use Illuminate\Database\Eloquent\Model as EloquentModel;
+use Neon\Model\Scopes\ActiveScope;
+
+class Base extends EloquentModel
+{
+  /** Define basic status values.
+   */
+  const STATUS_ACTIVE     = 'A'; // Active
+  const STATUS_DRAFT      = 'D'; // Draft
+  const STATUS_INACTIVE   = 'I'; // Inactive
+  const STATUS_NEW        = 'N'; // New
+
+  /** Define the default status.
+   * @const STATUS_DEFAULT The default status.
+   */
+  const STATUS_DEFAULT = self::STATUS_NEW;
+
+  /**
+   * The "booted" method of the model.
+   * 
+   * @see https://laravel.com/docs/9.x/eloquent#applying-global-scopes
+   *
+   * @return void
+   */
+  protected static function booted()
+  {
+    // static::addGlobalScope(new ActiveScope);
+  }
+
+  /**
+   * Get the name of the "status" column.
+   *
+   * @return string
+   */
+  public function getStatusColumn()
+  {
+    return defined(static::class . '::STATUS') ? static::STATUS : 'status';
+  }
+
+  /**
+   * Get the fully qualified "expired at" column.
+   *
+   * @return string
+   */
+  public function getQualifiedStatusColumn()
+  {
+    return $this->qualifyColumn($this->getStatusColumn());
+  }
+}
